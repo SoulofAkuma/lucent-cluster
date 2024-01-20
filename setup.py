f01a6c255390ce1a90352ba42a3331a1bc8e2d6b
SoulOfAkuma <allertmika@gmail.com>
2024-01-20 23:56:28 +0100

    adjusted setup.py because of encoding issue on install

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import setup, find_packages
 
 version = lucent.__version__
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
     name="torch-lucent",
     packages=find_packages(exclude=[]),
@@ -15,21 +12,7 @@ setup(
         "Collection of infrastructure and tools for research in "
         "neural network interpretability."
     ),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="The Lucent Authors",
-    author_email="limsweekiat@gmail.com",
-    url="https://github.com/greentfrapp/lucent",
     license="Apache License 2.0",
-    keywords=[
-        "pytorch",
-        "tensor",
-        "machine learning",
-        "neural networks",
-        "convolutional neural networks",
-        "feature visualization",
-        "optimization",
-    ],
     install_requires=[
         "torch>=1.5.0",
         "torchvision",
@@ -45,20 +28,5 @@ setup(
         "coverage",
         "coveralls",
         "scikit-learn"
-    ],
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: Education",
-        "License :: OSI Approved :: Apache Software License",
-        "Natural Language :: English",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Topic :: Scientific/Engineering :: Mathematics",
-        "Topic :: Scientific/Engineering :: Visualization",
-        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
